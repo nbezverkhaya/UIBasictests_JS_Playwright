1a790abf9d59e1f7303beae8bfc4b9986a71c5fa
nbezverkhaya <nbezverkhaya@gmail.com>
2025-05-17 22:44:13 +0200

    end to end test finished

--- tests/endToEnd.spec.js
+++ tests/endToEnd.spec.js
@@ -36,18 +36,18 @@ test.only('Client app login', async ({page}) =>
     await allProductsInCart.first().waitFor();
 
     // the first way to find the added product in the cart:
-    const countAddedProducts = await allProductsInCart.count();
-    for(let i = 0; i<countAddedProducts; ++i)
-    {
-        if(await allProductsInCart.nth(i).locator('h3').textContent() === productName)
-        {
-            console.log(productName, "is added");
-            break;
-        }
-    }
+    // const countAddedProducts = await allProductsInCart.count();
+    // for(let i = 0; i<countAddedProducts; ++i)
+    // {
+    //     if(await allProductsInCart.nth(i).locator('h3').textContent() === productName)
+    //     {
+    //         console.log(productName, "is added");
+    //         break;
+    //     }
+    // }
     // the second way to find the added product in the cart:
-    // const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
-    // expect(bool).toBeTruthy();
+    const bool = await page.locator(`h3:has-text("${productName}")`).isVisible();
+    expect(bool).toBeTruthy();
 
     const checkoutButton = page.locator('.btn.btn-primary').last();
     await checkoutButton.click();
@@ -70,6 +70,42 @@ test.only('Client app login', async ({page}) =>
         }
     }
 
+    const userFieldLabel = page.locator('.user__name label');
+    const userFieldInput = page.locator('.user__name input').first();
+    expect(userFieldLabel).toHaveText(myEmail);
+    await userFieldInput.fill(myEmail);
+    const placeOrder = page.locator('.action__submit');
+    await placeOrder.click();
+
+    const orderConfirmation = page.locator('.hero-primary');
+    await expect(orderConfirmation).toHaveText("Thankyou for the order.");
+    // await expect(orderConfirmation).toContainText("Thankyou for the order.");
+    const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
+    // console.log(orderId);
+    const ordersButton = page.locator('.btn.btn-custom').nth(1);
+    await ordersButton.click();
+
+    await page.locator('tbody').waitFor();
+    const rows = page.locator('tbody tr');
+
+    for (let i = 0; i < await rows.count(); ++i)
+    {
+        const rowOderId = await rows.nth(i).locator('th').textContent();
+        // console.log(rowOderId);
+        
+        if (orderId.includes(rowOderId));
+        {
+            await rows.nth(i).locator('button').first().click();
+            break;
+        }
+    }  
+
+    const orderIdDetails = await page.locator('.col-text').textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+
+    
+
+    
 
     await page.pause();
 
