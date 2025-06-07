c253cb8e95a478ef1fad694aabd4827991626001
nbezverkhaya <nbezverkhaya@gmail.com>
2025-06-07 22:43:57 +0200

    Add order creation request with token authorization

--- tests/webApiPart1.spec.js
+++ tests/webApiPart1.spec.js
@@ -1,90 +1,47 @@
 const {test, expect, request} = require('@playwright/test');
 
+const loginPayLoad = {userEmail: "75vkfjcf@gmail.com",userPassword: "Test123321!"};
+const orderPayLoad = {orders:[{country:"Honduras",productOrderedId:"67a8df1ac0d3e6622a297ccb"}]};
+let token;
+let orderId;
+
 // test.beforeEach( () => {});
 
-const loginPayLoad = {
-    userEmail: "75vkfjcf@gmail.com",
-    userPassword: "Test123321!"
-};
-
-let token;
-let myEmail = loginPayLoad.userEmail;
-
-test.beforeAll( async() =>
-{
+test.beforeAll(async () => {
     const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login', {data: loginPayLoad});
+    // 1. Login
+    const loginResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login',
+                                                {data: loginPayLoad});
+    //200,201,202 ...
     expect(loginResponse.ok()).toBeTruthy();
     const loginResponseJson = await loginResponse.json();
     token = loginResponseJson.token;
     console.log('Token:', token);
+
+    // 2. Create an order
+    const orderResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/order/create-order',
+    {
+        data: orderPayLoad,
+        headers:{
+            'Authorization' :token,
+            'Content-Type' : 'application/json'
+                }
+    });
+
+    const orderResponseJson = await orderResponse.json();
+    console.log(orderResponseJson);
+    orderId = orderResponseJson.orders[0];
+
 });
 
-test('Login with token', async ({page}) =>
-{
+test.only('Place an order', async ({page}) =>
+{   
     page.addInitScript(value => {
-        window.localStorage.setItem('token', value);
+        window.localStorage.setItem('token',value);
     }, token);
 
-    const productName = 'ZARA COAT 3';
-    page.goto('https://rahulshettyacademy.com/client/');
-    await page.locator(".card-body b").first().waitFor();
-    const products = page.locator(".card-body b");
-    const titles = await products.allTextContents();
-    console.log(titles);
-    const count = await products.count();
-    const productCards = page.locator(".card-body");
+    await page.goto('https://rahulshettyacademy.com/client');
 
-    for(let i = 0; i <count; ++i)
-    {
-    if (await productCards.nth(i).locator('b').textContent() === productName)
-    {
-        await productCards.nth(i).locator('text="Add To Cart"').click();
-        break;
-    }
-    }
-    const allCartButtons = page.locator('.fa.fa-shopping-cart');
-    await allCartButtons.first().click();
-
-    const allProductsInCart = page.locator('.cartSection');
-    await allProductsInCart.first().waitFor();
-
-    const bool = await page.locator(`h3:has-text("${productName}")`).isVisible();
-    expect(bool).toBeTruthy();
-
-    const checkoutButton = page.locator('.btn.btn-primary').last();
-    await checkoutButton.click();
-
-    const countryInput = page.locator('input[placeholder="Select Country"]');
-    await countryInput.pressSequentially('ukr');
-
-    const countriesDropdown = page.locator('.ta-results');
-    await countriesDropdown.waitFor();
-
-
-    const options = countriesDropdown.locator('button');
-    const optionsCount = await options.count();
-
-    for (let i = 0; i < optionsCount; ++i) {
-        const countryText = await options.nth(i).textContent();
-        if (countryText.trim() === "Ukraine") {
-            await options.nth(i).click();
-            break;
-        }
-    }
-
-    const userFieldLabel = page.locator('.user__name label');
-    const userFieldInput = page.locator('.user__name input').first();
-    expect(userFieldLabel).toHaveText(myEmail);
-    await userFieldInput.fill(myEmail);
-    const placeOrder = page.locator('.action__submit');
-    await placeOrder.click();
-
-    const orderConfirmation = page.locator('.hero-primary');
-    await expect(orderConfirmation).toHaveText("Thankyou for the order.");
-    // await expect(orderConfirmation).toContainText("Thankyou for the order.");
-    const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
-    // console.log(orderId);
     const ordersButton = page.locator('.btn.btn-custom').nth(1);
     await ordersButton.click();
 
@@ -104,8 +61,9 @@ test('Login with token', async ({page}) =>
     }  
 
     const orderIdDetails = await page.locator('.col-text').textContent();
+    await page.pause();
     expect(orderId.includes(orderIdDetails)).toBeTruthy();
 
     // await page.pause();
 
-});
+})
