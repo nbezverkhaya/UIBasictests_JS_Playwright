3e782d7ab5d567630ba3688eef0a8cab795270f1
nbezverkhaya <nbezverkhaya@gmail.com>
2025-06-12 22:38:24 +0200

    ApiUtils added

--- tests/webApiPart1.spec.js
+++ tests/webApiPart1.spec.js
@@ -1,44 +1,24 @@
 const {test, expect, request} = require('@playwright/test');
+const {APIUtils} = require('./utils/APIUtils');
 
 const loginPayLoad = {userEmail: "75vkfjcf@gmail.com",userPassword: "Test123321!"};
 const orderPayLoad = {orders:[{country:"Honduras",productOrderedId:"67a8df1ac0d3e6622a297ccb"}]};
-let token;
-let orderId;
+let response;
 
-// test.beforeEach( () => {});
-
-test.beforeAll(async () => {
+test.beforeAll(async()=>
+{
     const apiContext = await request.newContext();
-    // 1. Login
-    const loginResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login',
-                                                {data: loginPayLoad});
-    //200,201,202 ...
-    expect(loginResponse.ok()).toBeTruthy();
-    const loginResponseJson = await loginResponse.json();
-    token = loginResponseJson.token;
-    console.log('Token:', token);
+    const apiUtils = new APIUtils(apiContext, loginPayLoad);
+    response = await apiUtils.createOrder(orderPayLoad);
+})
 
-    // 2. Create an order
-    const orderResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/order/create-order',
-    {
-        data: orderPayLoad,
-        headers:{
-            'Authorization' :token,
-            'Content-Type' : 'application/json'
-                }
-    });
+test('Place an order', async ({page}) =>
 
-    const orderResponseJson = await orderResponse.json();
-    console.log(orderResponseJson);
-    orderId = orderResponseJson.orders[0];
-
-});
-
-test.only('Place an order', async ({page}) =>
 {   
-    page.addInitScript(value => {
-        window.localStorage.setItem('token',value);
-    }, token);
+    page.addInitScript(value =>
+    {
+    window.localStorage.setItem('token',value);
+    }, response.token);
 
     await page.goto('https://rahulshettyacademy.com/client');
 
@@ -53,7 +33,7 @@ test.only('Place an order', async ({page}) =>
         const rowOderId = await rows.nth(i).locator('th').textContent();
         // console.log(rowOderId);
         
-        if (orderId.includes(rowOderId))
+        if (response.orderId.includes(rowOderId))
         {
             await rows.nth(i).locator('button').first().click();
             break;
@@ -62,7 +42,7 @@ test.only('Place an order', async ({page}) =>
 
     const orderIdDetails = await page.locator('.col-text').textContent();
     await page.pause();
-    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    expect(response.orderId.includes(orderIdDetails)).toBeTruthy();
 
     // await page.pause();
 
